c767790a516bd6c41e9af4a3b38100ae2d09e06b
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-04-26 12:47:57 -0400

    Initialize main PHP file

---
+++ kaswara-replacement.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:       Kaswara Replacement
+ * Plugin URI:        https://angeloakcreative.com/
+ * Description:       Replaces lightweight functionality from the Kaswara plugin.
+ * Version:           0.1.0
+ * Author:            Angel Oak Creative, LLC
+ * Author URI:        https://angeloakcreative.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       kaswara-replacement
+ */
