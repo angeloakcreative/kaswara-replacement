9971575f9ee009125a8d8d363b59a48e20ab8a9c
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-04-26 17:48:58 -0400

    On hover of parent tag, toggle show and hide correct image. Update version.

--- js/kasrep_simple_hover.js
+++ js/kasrep_simple_hover.js
@@ -1,7 +1,14 @@
-// File version: 0.1.1
+// File version: 0.1.2
 
 jQuery(document).ready(function($) {
     // Hide mouseover class on document load, and show first image on document load
     $('.kasrep_simple_hover_image_before').show();
     $('.kasrep_simple_hover_image_after').hide();
+
+    // On hover of anchor tag of hover images, toggle show and hide on appropriate images
+    $('.kasrep_simple_hover_image_before').parent().hover(function(e) {
+        $('.kasrep_simple_hover_image_before').toggle();
+        $('.kasrep_simple_hover_image_after').toggle();
+    });
+
 });
