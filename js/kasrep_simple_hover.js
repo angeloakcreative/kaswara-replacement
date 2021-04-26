c94d37c530db95824887506abb499f86440c6906
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-04-26 17:57:54 -0400

    Update hover function to only manipulate specific selection, not all elements that match class

--- js/kasrep_simple_hover.js
+++ js/kasrep_simple_hover.js
@@ -1,4 +1,4 @@
-// File version: 0.1.2
+// File version: 0.1.3
 
 jQuery(document).ready(function($) {
     // Hide mouseover class on document load, and show first image on document load
@@ -7,8 +7,8 @@ jQuery(document).ready(function($) {
 
     // On hover of anchor tag of hover images, toggle show and hide on appropriate images
     $('.kasrep_simple_hover_image_before').parent().hover(function(e) {
-        $('.kasrep_simple_hover_image_before').toggle();
-        $('.kasrep_simple_hover_image_after').toggle();
+        $(this).find('.kasrep_simple_hover_image_before').toggle();
+        $(this).find('.kasrep_simple_hover_image_after').toggle();
     });
 
 });
