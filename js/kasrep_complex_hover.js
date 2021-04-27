fee78dd0788f38bea2471290a570f8fed86a2d16
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-04-26 20:48:38 -0400

    Hide overlay and overlay title on document load. Start hover event handler.

--- js/kasrep_complex_hover.js
+++ js/kasrep_complex_hover.js
@@ -1,6 +1,12 @@
-// File version: 0.1.0
+// File version: 0.1.1
 
 jQuery(document).ready(function($) {
+    $('.kasrep_complex_hover_overlay').hide();
+    $('.kasrep_complex_hover_title_top').hide();
 
+    // Handle hover event
+    $('.kasrep_complex_hover_container').hover(function(e) {
+
+    });
 
 });
