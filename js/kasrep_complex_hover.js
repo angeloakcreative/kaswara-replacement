f9672d0d5cb3e9a06c576f0ca84ed8472b032351
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-04-26 21:02:41 -0400

    Add comments. Add toggles to hover event handler. Update version number.

--- js/kasrep_complex_hover.js
+++ js/kasrep_complex_hover.js
@@ -1,12 +1,14 @@
-// File version: 0.1.1
+// File version: 0.1.2
 
 jQuery(document).ready(function($) {
+    // Hide overlay and top title on Document load
     $('.kasrep_complex_hover_overlay').hide();
     $('.kasrep_complex_hover_title_top').hide();
 
     // Handle hover event
     $('.kasrep_complex_hover_container').hover(function(e) {
-
+        $(this).toggleClass('kasrep_complex_hover_container_shadow');
+        $('.kasrep_complex_hover_overlay').toggle();
+        $('.kasrep_complex_hover_title_top').toggle();
     });
-
 });
