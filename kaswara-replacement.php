7e314d7cc0101bea347543bd1fb6a9abaefe6826
Sunjay Armstead <sunjay@angeloakcreative.com>
2021-04-26 22:25:55 -0400

    Add quote marks to all html attributes in simple hover shortcode function. Update plugin version number.

--- kaswara-replacement.php
+++ kaswara-replacement.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Kaswara Replacement
  * Plugin URI:        https://angeloakcreative.com/
  * Description:       Replaces lightweight functionality from the Kaswara plugin.
- * Version:           0.1.19
+ * Version:           0.1.20
  * Author:            Angel Oak Creative, LLC
  * Author URI:        https://angeloakcreative.com/
  * License:           GPL v2 or later
@@ -41,21 +41,21 @@ add_action('wp_enqueue_scripts', 'kasrep_enqueue');
 
 
 add_shortcode('kasrep_simple_hover', 'kasrep_simple_hover');
-function kasrep_simple_hover( $atts, $content, $shortcode_tag ) {
+function kasrep_simple_hover( $atts = [], $content, $shortcode_tag ) {
     /* Define attributes array */
     $atts = shortcode_atts( array(
         'image_one_src' => '',
         'image_one_alt' => 'Hover image',
         'image_two_src' => '',
-        'image_one_alt' => 'Hover image',
+        'image_two_alt' => 'Hover image',
         'href' => '',
-        'target_new_page' => '1',
+        'target_new_page' => '1'
     ), $atts, 'kasrep_simple_hover');
 
     $img_one_src = esc_url($atts["image_one_src"]);
-    $img_one_alt = ($atts["image_one_alt"]);
+    $img_one_alt = esc_attr($atts["image_one_alt"]);
     $img_two_src = esc_url($atts["image_two_src"]);
-    $img_two_alt = ($atts["image_two_alt"]);
+    $img_two_alt = esc_attr($atts["image_two_alt"]);
     $href = esc_url($atts["href"]);
 
     /* Define $content conditionally */
@@ -63,14 +63,14 @@ function kasrep_simple_hover( $atts, $content, $shortcode_tag ) {
         $target = '_blank';
         $rel = 'noopener noreferrer';
 
-        $content = "<a href=$href target=$target rel=$rel>";
-        $content .= "<img class='kasrep_simple_hover_image_before' src=$img_one_src alt=$img_one_alt />";
+        $content = "<a href='$href' target='$target' rel='$rel'>";
+        $content .= "<img class='kasrep_simple_hover_image_before' src='$img_one_src' alt='$img_one_alt' />";
         $content .= "<img class='kasrep_simple_hover_image_after' src=$img_two_src alt=$img_two_alt />";
         $content .="</a>";
     } else {
-        $content = "<a href=$href>";
-        $content .= "<img class='kasrep_simple_hover_image_before' src=$img_one_src alt=$img_one_alt />";
-        $content .= "<img class='kasrep_simple_hover_image_after' src=$img_two_src alt=$img_two_alt />";
+        $content = "<a href='$href'>";
+        $content .= "<img class='kasrep_simple_hover_image_before' src='$img_one_src' alt='$img_one_alt' />";
+        $content .= "<img class='kasrep_simple_hover_image_after' src='$img_two_src' alt='$img_two_alt' />";
         $content .="</a>";
     }
 
